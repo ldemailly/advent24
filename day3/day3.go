0ef1e1ead4c0dc16d4e86a86e0b7cabdfee4b014
Laurent Demailly <ldemailly@gmail.com>
2024-12-02 22:30:12 -0800

    day3 part 2

--- day3/day3.go
+++ day3/day3.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"regexp"
 	"strconv"
+	"strings"
 )
 
 func main() {
@@ -13,8 +14,28 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	dataStr := string(data)
+	Mul(dataStr)
+
+	// Part 2
+	for {
+		idx1 := strings.Index(dataStr, "don't()")
+		if idx1 == -1 {
+			break
+		}
+		idx2 := strings.Index(dataStr[idx1:], "do()")
+		if idx2 == -1 {
+			dataStr = dataStr[:idx1]
+			break
+		}
+		dataStr = dataStr[:idx1] + dataStr[idx1+idx2+4:]
+	}
+	Mul(dataStr)
+}
+
+func Mul(data string) {
 	re := regexp.MustCompile(`mul\(([0-9]+),([0-9]+)\)`)
-	res := re.FindAllStringSubmatch(string(data), -1)
+	res := re.FindAllStringSubmatch(data, -1)
 	sum := 0
 	for _, r := range res {
 		a, b := r[1], r[2]
