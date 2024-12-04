203285056d3c849cfcb1788b67a10d4bcf86e5e7
Laurent Demailly <ldemailly@gmail.com>
2024-12-04 10:59:51 -0800

    Day 4 part2

--- day4/day4.go
+++ day4/day4.go
@@ -24,6 +24,22 @@ func HasXMAS(matrix [][]string, i, j, deltaX, deltaY int) int {
 	return 1
 }
 
+func HasMASCrossed(matrix [][]string, i, j int) bool {
+	if matrix[i-1][j-1] != "M" && matrix[i-1][j-1] != "S" {
+		return false
+	}
+	if matrix[i+1][j-1] != "M" && matrix[i+1][j-1] != "S" {
+		return false
+	}
+	if matrix[i-1][j+1] != "M" && matrix[i-1][j+1] != "S" {
+		return false
+	}
+	if matrix[i+1][j+1] != "M" && matrix[i+1][j+1] != "S" {
+		return false
+	}
+	return matrix[i-1][j-1] != matrix[i+1][j+1] && matrix[i-1][j+1] != matrix[i+1][j-1]
+}
+
 func main() {
 	data, err := io.ReadAll(os.Stdin)
 	if err != nil {
@@ -66,4 +82,19 @@ func main() {
 		}
 	}
 	fmt.Println(sum)
+
+	// Part 2
+	sum = 0
+	for i := 1; i < len(matrix)-1; i++ {
+		for j := 1; j < len(matrix[i])-1; j++ {
+			if matrix[i][j] != "A" {
+				continue
+			}
+			if HasMASCrossed(matrix, i, j) {
+				sum++
+			}
+		}
+	}
+	fmt.Println(sum)
+
 }
