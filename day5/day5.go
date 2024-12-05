988aea06093f72aba1446d87470140acdb135ec8
Laurent Demailly <ldemailly@gmail.com>
2024-12-05 11:33:55 -0800

    Day5: part 2 brute force bubbling to right spot

--- day5/day5.go
+++ day5/day5.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 	"slices"
 	"strconv"
@@ -16,9 +17,12 @@ type Rule struct {
 }
 
 type Data struct {
-	Row    []int // raw row
-	Set    sets.Set[int]
-	Middle int
+	Row []int // raw row
+	Set sets.Set[int]
+}
+
+func (d Data) Middle() int {
+	return d.Row[len(d.Row)/2]
 }
 
 func main() {
@@ -52,13 +56,13 @@ func main() {
 			row.Row = append(row.Row, v)
 		}
 		row.Set = sets.FromSlice(row.Row)
-		row.Middle = row.Row[len(row.Row)/2]
 		data = append(data, row)
 	}
 	// fmt.Println(data)
 
-	// Part 1
-	sum := 0
+	// Part 1 + Part 2 prep
+	sum1 := 0
+	var badRows []Data
 	for i, row := range data {
 		rowOk := true
 		for j, r := range rules {
@@ -68,16 +72,53 @@ func main() {
 			}
 			i1 := slices.Index(row.Row, r.Before)
 			i2 := slices.Index(row.Row, r.After)
+			if i1 == -1 || i2 == -1 {
+				log.Fatalf("BUG Row %d: Rule %d: %d or %d not found in %v despite check", i+1, j+1, r.Before, r.After, row)
+			}
 			if i1 > i2 {
-				fmt.Printf("Row %d elimited by rule %d: %v: %d is before %d\n", i+1, j+1, row.Row, r.Before, r.After)
+				fmt.Printf("Row %d eliminated by rule %d: %v: %d not before %d\n", i+1, j+1, row.Row, r.Before, r.After)
 				rowOk = false
 				break
 			}
 		}
 		if rowOk {
-			fmt.Printf("Row %d is ok, adding %d: %v\n", i+1, row.Middle, row.Row)
-			sum += row.Middle
+			fmt.Printf("Row %d is ok, adding %d: %v\n", i+1, row.Middle(), row.Row)
+			sum1 += row.Middle()
+		} else {
+			badRows = append(badRows, row)
 		}
 	}
-	fmt.Println(sum)
+	fmt.Println(sum1)
+	sum2 := 0
+	for i, row := range badRows {
+	recheck:
+		didFix := false
+		for j, r := range rules {
+			if !(row.Set.Has(r.Before) && row.Set.Has(r.After)) {
+				continue
+			}
+			for {
+				i1 := slices.Index(row.Row, r.Before)
+				i2 := slices.Index(row.Row, r.After)
+				if i1 == -1 || i2 == -1 {
+					log.Fatalf("BUG Row %d: Rule %d: %d or %d not found in %v", i+1, j+1, r.Before, r.After, row)
+				}
+				if i1 < i2 {
+					break
+				}
+				// move the after to the left
+				fmt.Printf("Row %d fixing for rule %d (%d before %d): before %v -> ", i+1, j+1, r.Before, r.After, row.Row)
+				row.Row[i1], row.Row[i1-1] = row.Row[i1-1], row.Row[i1]
+				fmt.Printf("now %v\n", row.Row)
+				didFix = true
+			}
+		}
+		if didFix {
+			// extra pass to recheck
+			fmt.Printf("Rechecking %d as we made changes\n", i+1)
+			goto recheck
+		}
+		sum2 += row.Middle()
+	}
+	fmt.Println(sum2)
 }
