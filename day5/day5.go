050683df6ac3029f01cfcab25f1cc7f8e133b39e
Laurent Demailly <ldemailly@gmail.com>
2024-12-05 10:41:23 -0800

    Day 5: getting there, using sets and running the rules

--- day5/day5.go
+++ day5/day5.go
@@ -4,18 +4,26 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"slices"
 	"strconv"
 	"strings"
+
+	"fortio.org/sets"
 )
 
 type Rule struct {
 	Before, After int
 }
 
+type Data struct {
+	Row []int // raw row
+	Set sets.Set[int]
+}
+
 func main() {
 	s := bufio.NewScanner(os.Stdin)
 	var rules []Rule
-	fmt.Println("Rules:")
+	// fmt.Println("Rules:")
 	for s.Scan() {
 		line := s.Text()
 		if line == "" {
@@ -28,21 +36,43 @@ func main() {
 		}
 		rules = append(rules, r)
 	}
-	fmt.Println(rules)
-	fmt.Println("Data:")
-	var data [][]int
+	// fmt.Println(rules)
+	// fmt.Println("Data:")
+	var data []Data
 	for s.Scan() {
 		line := s.Text()
 		elems := strings.Split(line, ",")
-		var row []int
+		var row Data
 		for _, e := range elems {
 			v, err := strconv.Atoi(e)
 			if err != nil {
 				panic(err)
 			}
-			row = append(row, v)
+			row.Row = append(row.Row, v)
 		}
+		row.Set = sets.FromSlice(row.Row)
 		data = append(data, row)
 	}
-	fmt.Println(data)
+	// fmt.Println(data)
+
+	// Part 1
+	for i, row := range data {
+		rowOk := true
+		for j, r := range rules {
+			if !(row.Set.Has(r.Before) && row.Set.Has(r.After)) {
+				// need both to be there for the rule to matter
+				continue
+			}
+			i1 := slices.Index(row.Row, r.Before)
+			i2 := slices.Index(row.Row, r.After)
+			if i1 > i2 {
+				fmt.Printf("Row %d elimited by rule %d: %v: %d is before %d\n", i+1, j+1, row.Row, r.Before, r.After)
+				rowOk = false
+				break
+			}
+		}
+		if rowOk {
+			fmt.Printf("Row %d is ok: %v\n", i+1, row.Row)
+		}
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/ldemailly/advent24
 
 go 1.23.3
+
+require fortio.org/sets v1.2.0
+
+require golang.org/x/exp v0.0.0-20240604190554-fc45aab8b7f8 // indirect
