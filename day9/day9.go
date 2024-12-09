34807bd3ac3803eea2d59b994981c38fbf52cb6c
Laurent Demailly <ldemailly@gmail.com>
2024-12-08 22:41:24 -0800

    Flattened and defragged version

--- day9/day9.go
+++ day9/day9.go
@@ -20,14 +20,18 @@ const EMPTY = -1
 
 type FS struct {
 	s      []File // fileids
+	size   int
 	lastId int
+	flat   []int
 }
 
 func (fs *FS) NewEmpty(len int) {
+	fs.size += len
 	fs.s = append(fs.s, File{id: EMPTY, size: len})
 }
 
 func (fs *FS) NewFile(len int) {
+	fs.size += len
 	fs.s = append(fs.s, File{id: fs.lastId, size: len})
 	fs.lastId++
 }
@@ -55,6 +59,47 @@ func CharToLen(c byte) int {
 	return int(c - '0')
 }
 
+func (fs *FS) Flatten() {
+	fs.flat = make([]int, fs.size)
+	i := 0
+	for _, f := range fs.s {
+		for range f.size {
+			fs.flat[i] = f.id
+			i++
+		}
+	}
+}
+
+func (fs *FS) Defrag() {
+	i := 0
+	j := fs.size - 1
+	for i <= j {
+		if fs.flat[j] == EMPTY {
+			j--
+			continue
+		}
+		if fs.flat[i] != EMPTY {
+			i++
+			continue
+		}
+		fs.flat[i], fs.flat[j] = fs.flat[j], fs.flat[i]
+		i++
+		j--
+	}
+}
+
+func (fs *FS) FlatString() string {
+	var buf strings.Builder
+	for _, id := range fs.flat {
+		if id == EMPTY {
+			buf.WriteByte('.')
+		} else {
+			buf.WriteByte(byte(id + '0'))
+		}
+	}
+	return buf.String()
+}
+
 func main() {
 	fs := FS{}
 	m, _ := io.ReadAll(os.Stdin)
@@ -68,6 +113,10 @@ func main() {
 			fs.NewEmpty(CharToLen(c))
 		}
 	}
-	fmt.Println(fs)
-
+	fmt.Printf("FS is %d blocks (%s)\n", fs.size, fs)
+	fs.Flatten()
+	fmt.Printf("Flattened is %s (%v)\n", fs.FlatString(), fs.flat)
+	fs.Defrag()
+	fmt.Printf("Defragged is %s (%v)\n", fs.FlatString(), fs.flat)
+	// fmt.Println("Part1:", fs.Checksum())
 }
