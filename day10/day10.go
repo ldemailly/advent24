69397eae9884dfeb29703bf12e0cfc00c01d153f
Laurent Demailly <ldemailly@gmail.com>
2024-12-10 00:06:08 -0800

    Color all the paths

--- day10/day10.go
+++ day10/day10.go
@@ -22,6 +22,11 @@ var ansi256Gray = []int{
 	15,  // 255 white
 }
 
+// copied from  https://pkg.go.dev/fortio.org/terminal fps -fire mode
+//
+// --------------------------- 0   1   2   3    4    5    6    7    8    9.
+var ansiColorGradiant = []int{52, 88, 124, 166, 202, 208, 214, 220, 226, 228}
+
 const (
 	GreenBackground = "\033[42m"
 	// Copied/inspired from my https://pkg.go.dev/fortio.org/terminal/ansipixels
@@ -34,7 +39,7 @@ const (
 	ClearEndOfLine = "\033[K"
 )
 
-func HeightToPixel(h int) string {
+func HeightToPixel(h int, color bool) string {
 	if h < 0 || h >= len(ansi256Gray) {
 		log.Fatalf("Invalid height %d", h)
 	}
@@ -42,29 +47,79 @@ func HeightToPixel(h int) string {
 	if h < 5 {
 		fg = 15
 	}
-	return fmt.Sprintf("\033[48;5;%dm\033[38;5;%dm %d\033[0m", ansi256Gray[h], fg, h)
+	c := ansi256Gray[h]
+	if color {
+		c = ansiColorGradiant[h]
+	}
+	return fmt.Sprintf("\033[48;5;%dm\033[38;5;%dm %d\033[0m", c, fg, h)
+}
+
+type Point struct {
+	height int
+	path   bool
 }
 
 // happens to be square.
 type Map struct {
-	heights [][]int
-	l       int
+	points [][]Point
+	l      int
 }
 
 func (m *Map) Print() string {
 	var b strings.Builder
 	// b.WriteString("\033[H")
 	for y := range m.l {
-		line := m.heights[y]
+		line := m.points[y]
 		for x := range m.l {
-			b.WriteString(HeightToPixel(line[x]))
+			b.WriteString(HeightToPixel(line[x].height, line[x].path))
 		}
 		b.WriteString("\n")
 	}
 	return b.String()
 }
 
-var doPrint = true
+func (m *Map) SetPaths() {
+	sum := 0
+	// find all the starts
+	for y := range m.l {
+		for x := range m.l {
+			if m.points[y][x].height == 0 {
+				p := m.CheckPath(x, y, 0)
+				sum += p
+				if p > 0 {
+					fmt.Printf("Found %d path(s) at %d %d\n", p, x, y)
+				}
+			}
+		}
+	}
+	fmt.Printf("Found %d paths\n", sum)
+}
+
+func (m *Map) CheckPath(x, y, cur int) int {
+	// fmt.Printf("Checking %d %d %d\n", x, y, cur)
+	if x < 0 || x >= m.l || y < 0 || y >= m.l {
+		return 0
+	}
+	h := m.points[y][x].height
+	if h != cur {
+		return 0
+	}
+	if h == 9 {
+		m.points[y][x].path = true
+		return 1
+	}
+	cur++
+	// check all not just first match
+	p := m.CheckPath(x+1, y, cur)
+	p += m.CheckPath(x, y-1, cur)
+	p += m.CheckPath(x-1, y, cur)
+	p += m.CheckPath(x, y+1, cur)
+	if p > 0 {
+		m.points[y][x].path = true
+		return p
+	}
+	return 0
+}
 
 func main() {
 	//	flag.BoolVar(&doPrint, "print", false, "Print the map")
@@ -75,24 +130,21 @@ func main() {
 	for s.Scan() {
 		line := s.Text()
 		elems := strings.Split(line, "")
-		var row []int
+		var row []Point
 		for _, e := range elems {
 			n, err := strconv.Atoi(e)
 			if err != nil {
 				log.Fatalf("Invalid height %q: %v", e, err)
 			}
-			row = append(row, n)
+			row = append(row, Point{height: n})
 		}
-		m.heights = append(m.heights, row)
+		m.points = append(m.points, row)
 		y++
 	}
-	m.l = len(m.heights)
-	if len(m.heights[0]) != m.l {
-		log.Fatalf("Invalid matrix %d vs %d", len(m.heights[0]), m.l)
+	m.l = len(m.points)
+	if len(m.points[0]) != m.l {
+		log.Fatalf("Invalid matrix %d vs %d", len(m.points[0]), m.l)
 	}
-	if doPrint {
-		// clear screen
-		// fmt.Print(ClearScreen)
-		fmt.Print(m.Print())
-	}
+	m.SetPaths()
+	fmt.Print(m.Print())
 }
