57080efb0fe83dd70274a9e3c7719fc58f907438
Laurent Demailly <ldemailly@gmail.com>
2024-12-12 10:12:43 -0800

    cleaned up part1 to use directions + non functional (wrong) part 2

--- day12/day12.go
+++ day12/day12.go
@@ -14,10 +14,18 @@ type Coord struct {
 	x, y int
 }
 
+/*
+type Edge struct {
+	direction Coord
+	where     Coord // only one of x or y is set depending on direction (ie for vertical, which line)
+}
+*/
+
 type Region struct {
 	id        byte
 	coords    sets.Set[Coord]
 	perimeter int
+	edges     int
 }
 
 type Map struct {
@@ -41,26 +49,71 @@ func (m *Map) Regions() {
 	}
 }
 
-func (m *Map) Edges(x, y int) int {
+var (
+	west  = Coord{-1, 0}
+	south = Coord{0, 1}
+	north = Coord{0, -1}
+	east  = Coord{1, 0}
+)
+
+// West, South, North, East // optimized for fill order.
+var directions = []Coord{west, south, north, east}
+
+func (c Coord) SwapAxis() Coord {
+	return Coord{c.y, c.x}
+}
+
+func (c Coord) SwapDirection() Coord {
+	return Coord{-c.x, -c.y}
+}
+
+func (m *Map) IsEdge(c byte, x, y int, direction Coord) bool {
+	xx := x + direction.x
+	yy := y + direction.y
+	return m.Outside(xx, yy) || m.plots[yy][xx] != c
+}
+
+func (m *Map) Perimeter(x, y int) int {
 	edges := 0
 	c := m.plots[y][x]
-	if x == 0 || m.plots[y][x-1] != c {
-		edges++
-	}
-	if y == 0 || m.plots[y-1][x] != c {
-		edges++
-	}
-	if x == len(m.plots[0])-1 || m.plots[y][x+1] != c {
-		edges++
-	}
-	if y == len(m.plots)-1 || m.plots[y+1][x] != c {
-		edges++
+	for _, d := range directions {
+		if m.IsEdge(c, x, y, d) {
+			edges++
+		}
 	}
 	return edges
 }
 
+func (m *Map) Edges(x, y int) int {
+	edges := 0
+	c := m.plots[y][x]
+	for _, d := range directions {
+		if !m.IsEdge(c, x, y, d) {
+			continue
+		}
+		n1d := d.SwapAxis()
+		n2d := n1d.SwapDirection()
+		x1 := x + n1d.x
+		y1 := y + n1d.y
+		x2 := x + n2d.x
+		y2 := y + n2d.y
+		fmt.Printf("Checking %c %d %d direction %v -> %d %d and %d %d : ", c, x, y, d, x1, y1, x2, y2)
+		if m.Outside(x1, y1) || m.Outside(x2, y2) || !m.IsEdge(c, x1, y1, d) || !m.IsEdge(c, x2, y2, d) {
+			fmt.Println("yes")
+			edges++
+		} else {
+			fmt.Println("no")
+		}
+	}
+	return edges
+}
+
+func (m *Map) Outside(x, y int) bool {
+	return x < 0 || y < 0 || x >= len(m.plots) || y >= len(m.plots[0])
+}
+
 func (m *Map) ExpandRegion(x, y int, c byte, r *Region) {
-	if x < 0 || y < 0 || x >= len(m.plots) || y >= len(m.plots[0]) {
+	if m.Outside(x, y) {
 		return
 	}
 	if m.plots[y][x] != c {
@@ -72,14 +125,15 @@ func (m *Map) ExpandRegion(x, y int, c byte, r *Region) {
 	}
 	r.coords.Add(co)
 	m.seen.Add(co)
-	m.ExpandRegion(x+1, y, c, r)
-	m.ExpandRegion(x, y+1, c, r)
-	m.ExpandRegion(x, y-1, c, r)
-	m.ExpandRegion(x-1, y, c, r)
-	edges := m.Edges(x, y)
-	r.perimeter += edges
-	if edges == 0 {
+	for _, d := range directions {
+		m.ExpandRegion(x+d.x, y+d.y, c, r)
+	}
+	perim := m.Perimeter(x, y)
+	r.perimeter += perim
+	if perim == 0 {
 		m.inner.Add(co)
+	} else {
+		r.edges += m.Edges(x, y)
 	}
 }
 
@@ -93,7 +147,8 @@ func Color(c, what byte) string {
 }
 
 func (m *Map) String() string {
-	sum := 0
+	sum1 := 0
+	sum2 := 0
 	var b strings.Builder
 	for y, row := range m.plots {
 		for x, c := range row {
@@ -108,11 +163,15 @@ func (m *Map) String() string {
 	for _, r := range m.regions {
 		s := len(r.coords)
 		p := r.perimeter
-		cost := s * p
-		sum += cost
-		b.WriteString(fmt.Sprintf("Region %c: s %d p %d = %d\n", r.id, s, p, cost))
+		cost1 := s * p
+		sum1 += cost1
+		e := r.edges
+		cost2 := s * e
+		sum2 += cost2
+		b.WriteString(fmt.Sprintf("Region %c: s %d p %d = %d - e %d = %d\n", r.id, s, p, cost1, e, cost2))
 	}
-	b.WriteString(fmt.Sprintf("Total: %d\n", sum))
+	b.WriteString(fmt.Sprintf("Total with perimeter : %d\n", sum1))
+	b.WriteString(fmt.Sprintf("Total with edges     : %d\n", sum2))
 	return b.String()
 }
 
